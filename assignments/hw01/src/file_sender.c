856df87e66ee1dae61524b8c9ce258d1e7b2393c
knedl1k <knedl1k@tuta.io>
2024-03-24 12:22:16 +0100

    syntax improvement

--- assignments/hw01/src/file_sender.c
+++ assignments/hw01/src/file_sender.c
@@ -14,54 +14,48 @@
 
 #define PACKET_MAX_LEN 1024
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
-}
+void error(const char *msg);
 
-int main(int argc, char *argv[]) {
-    if (argc < 5) {
+
+int main(int argc, char *argv[]){
+    if (argc < 5){
         fprintf(stderr,"Usage: %s <server_ip> <server_port> <filename> <client_port>\n", argv[0]);
         exit(1);
     }
 
-    char *server_ip = argv[1];
-    int server_port = atoi(argv[2]);
-    char *filename = argv[3];
-    int client_port = atoi(argv[4]);
+    const char *server_ip=argv[1];
+    const int server_port=atoi(argv[2]);
+    const char *filename=argv[3];
+    const int client_port=atoi(argv[4]);
 
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL) {
+    FILE *file=fopen(filename, "rb");
+    if (file == NULL)
         error("Error opening file");
-    }
-
+    
     int sockfd;
     struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
+    socklen_t client_len=sizeof(client_addr);
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
+    sockfd=socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
         error("Error opening socket");
-    }
-
+    
     memset(&client_addr, 0, sizeof(client_addr));
-    client_addr.sin_family = AF_INET;
-    client_addr.sin_addr.s_addr = INADDR_ANY;
-    client_addr.sin_port = htons(client_port);
+    client_addr.sin_family=AF_INET;
+    client_addr.sin_addr.s_addr=INADDR_ANY;
+    client_addr.sin_port=htons(client_port);
 
-    if (bind(sockfd, (struct sockaddr *) &client_addr, sizeof(client_addr)) < 0) {
+    if (bind(sockfd, (struct sockaddr *) &client_addr, sizeof(client_addr)) < 0)
         error("Error on binding");
-    }
-
+    
     memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(server_port);
-    if (inet_aton(server_ip, &server_addr.sin_addr) == 0) {
+    server_addr.sin_family=AF_INET;
+    server_addr.sin_port=htons(server_port);
+    if (inet_aton(server_ip, &server_addr.sin_addr) == 0)
         error("Invalid server IP address");
-    }
-
+    
     fseek(file, 0, SEEK_END);
-    long file_size = ftell(file);
+    const long file_size=ftell(file);
     fseek(file, 0, SEEK_SET);
 
     char send_buffer[PACKET_MAX_LEN];
@@ -70,8 +64,8 @@ int main(int argc, char *argv[]) {
     sprintf(send_buffer, "NAME=%s\nSIZE=%ld\nSTART\n", filename, file_size);
     sendto(sockfd, send_buffer, strlen(send_buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    while ((bytes_read = fread(send_buffer + 12, 1, PACKET_MAX_LEN - 12, file)) > 0) {
-        uint32_t position = htonl(ftell(file));
+    while((bytes_read=fread(send_buffer + 12, 1, PACKET_MAX_LEN - 12, file)) > 0){
+        uint32_t position=htonl(ftell(file));
         memcpy(send_buffer, &position, sizeof(uint32_t));
         sendto(sockfd, send_buffer, bytes_read + 12, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
     }
@@ -84,3 +78,7 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void error(const char *msg){
+    perror(msg);
+    exit(1);
+}
